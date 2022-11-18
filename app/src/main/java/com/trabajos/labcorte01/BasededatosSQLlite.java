50bf1ea1c855d855af82894c1cc824db7b732233
ANDRES BURGOS <elandres14>
2022-11-18 16:59:30 -0500

    VARIABLE ID (autoincremento)

--- app/src/main/java/com/trabajos/labcorte01/BasededatosSQLlite.java
+++ app/src/main/java/com/trabajos/labcorte01/BasededatosSQLlite.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 public class BasededatosSQLlite extends SQLiteOpenHelper {
     private static final String NOMBRE_DB=" BDSQLite";
     private static final int VERSION_DB=1;
-    private static final String TABLA_EVENTOS="CREATE TABLE EVENTOS(ID TEXT PRIMARY KEY, FechaIni TEXT, Descripcion TEXT, Causa TEXT, ServicioAfectado TEXT,FechaFin TEXT, Indisponibilidad TEXT)";
+    private static final String TABLA_EVENTOS="CREATE TABLE EVENTOS(ID TEXT INTEGER PRIMARY KEY AUTOINCREMENT , FechaIni TEXT, Descripcion TEXT, Causa TEXT, ServicioAfectado TEXT,FechaFin TEXT, Indisponibilidad TEXT)";
 
     public BasededatosSQLlite( Context context) {
         super(context, NOMBRE_DB, null , VERSION_DB);
